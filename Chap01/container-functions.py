1e0d7658e4a5e3e4b2598bde1265307e0409d61c
Bryan Siegel <bryansiegel@gmail.com>
2020-04-01 16:14:52 -0700

    finished course

---
+++ Chap01/container-functions.py
@@ -0,0 +1,14 @@
+import os
+import random
+import datetime
+
+def main():
+    # v = os.getcwd()
+    # v = list(range(25))
+    now = datetime.datetime.now()
+    print(now.year, now.month, now.day)
+    # random.shuffle(v)
+    # print(v)
+
+if __name__ == "__main__":
+    main()
