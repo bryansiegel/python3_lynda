96bae0c3cdbe91b7ba2f46e6533de517e8608455
Bryan Siegel <bryansiegel@gmail.com>
2020-04-06 13:12:22 -0700

    range and sorted functions

--- Chap01/data-structures.py
+++ Chap01/data-structures.py
@@ -1,15 +1,44 @@
 # list
 # is mutable (can change)
-x = [1, 2, 3, 4, 5]
+l = [1, 2, 3, 4, 5]
+ls = ['bryan', 'summer', 'haileigh', 'logun']
+# new_ls = sorted(ls)
+# print(new_ls)
+for i in range(len(ls)):
+    print(str(i) + " " + ls[i])
+# res = zip(l, ls)
+# print(list(res))
+
+# ls.append('home')
+# ls.sort(ls.reverse())
+# print(list(ls))
+
+# for lss in ls:
+    # print(lss +  lss.join(','))
+    # print(lss.strip('h'))
+    # print(lss.splitlines(False))
 
 # tuple
 # not mutable (cannot change once it's been created)
-x = (1, 2, 3, 4, 5)
+t = (1, 2, 3, 4, 5)
+ts = ('bryan', 'summer', 'haileight', 'logun')
+
+# print(type(ts))
+
+# for tpl in ts:
+#     print(tpl.upper())
+# print(type(tpl))
+    
 
 # dictionary
 # sequence of key value pairs
-x = {"a": 1, "b": 2, "c": 3}
+d = {"a": 1, "b": 2, "c": 3}
+# dictv = d.items()
+# print(d.get('a'))
+
 
 # set
 # unordered set of unique values
-x = {1, 2, 3, 4, 5}
+s = {1, 2, 3, 4, 5}
+
+# print(d)
